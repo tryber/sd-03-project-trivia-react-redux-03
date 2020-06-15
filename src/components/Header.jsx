715b1200273ce9ca176c2ad2b2f74c4aa666f712
Rhian Castro <rhi.castro@gmail.com>
2020-06-15 17:52:51 -0300

    code climate fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Link, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import hashedMail from '../services/encrypt_mail';
 import '../styles/Header.css';
 
