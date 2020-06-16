00945e4be634105643a4010a077239af94e5cb92
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-06-16 18:49:41 -0300

    Página de Ranking finalizada

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+function dataRanking() {
+  const data = JSON.parse(localStorage.getItem('ranking'));
+  data.sort((a, b) => (a.score - b.score));
+  return (
+    <div>
+      {data.map((item, index) => <div key={item.name}>
+        <img src={item.picture} />
+        <h4 data-testid={`player-name-${index}`}>Nome: {item.name}</h4>
+        <p data-testid={`player-score-${index}`}>Pontuação: {item.score}</p>
+      </div>)}
+    </div>
+  );
+}
+
 class Ranking extends React.Component {
-
-  dataRanking() {
-    const data = JSON.parse(localStorage.getItem('ranking'));
-    data.sort((a, b) => (a.score - b.score));
-    return (
-      <div>
-        {data.map((item, index) => <div key={index}>
-          <h4 data-testid={`player-name-${index}`}>Nome: {item.name}</h4>
-          <p data-testid={`player-score-${index}`}>Pontuação: {item.score}</p>
-        </div>)}
-      </div>
-    );
-  }
-
   render() {
     return (
       <div>
         <h1>Ranking</h1>
         <Link to="/" data-testid="btn-go-home">Início</Link>
-        {this.dataRanking()}
+        {dataRanking()}
       </div>
     );
   }
