7f0d8edd053a52f468b562b6dec54954de7b15e5
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-06-16 18:44:28 -0300

    Página de Ranking finalizada

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Ranking() {
-  return (
-    <div>
-      <h1>Ranking</h1>
-    </div>
-  );
+class Ranking extends React.Component {
+
+  dataRanking() {
+    const data = JSON.parse(localStorage.getItem('ranking'));
+    data.sort((a, b) => (a.score - b.score));
+    return (
+      <div>
+        {data.map((item, index) => <div key={index}>
+          <h4 data-testid={`player-name-${index}`}>Nome: {item.name}</h4>
+          <p data-testid={`player-score-${index}`}>Pontuação: {item.score}</p>
+        </div>)}
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ranking</h1>
+        <Link to="/" data-testid="btn-go-home">Início</Link>
+        {this.dataRanking()}
+      </div>
+    );
+  }
 }
 
 export default Ranking;
